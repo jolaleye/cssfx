aef54188dc849b4d6a359d1999bc9cd43d27fabc
jolaleye <j.olaleye.o@gmail.com>
2019-04-06 20:33:03 -0500

    Add header

---
+++ components/header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="container mx-auto text-center px-8 py-10 md:py-16">
+        <h1 className="font-display text-gray-800 text-2xl md:text-3xl">
+          Beautifully simple click-to-copy CSS effects
+        </h1>
+      </div>
+    </header>
+  );
+}
