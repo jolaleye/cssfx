fce9a4323a8594ced99563727687baf3cc59411e
jolaleye <j.olaleye.o@gmail.com>
2019-04-07 12:18:08 -0500

    Create EffectContainer

---
+++ components/EffectContainer.js
@@ -0,0 +1,7 @@
+export default function EffectContainer({ children }) {
+  return (
+    <div className="w-1/2 mb-4 px-2 sm:w-1/3 md:w-1/4 md:px-3 md:mb-6 lg:w-1/5 xl:px-4 xl:mb-8">
+      <div className="flex items-center justify-center h-32 hover:bg-gray-100 hover:cursor-pointer">{children}</div>
+    </div>
+  );
+}
--- components/effect.js
+++
@@ -1,7 +0,0 @@
-export default function Effect() {
-  return (
-    <div className="w-1/2 mb-4 px-2 sm:w-1/3 md:w-1/4 md:px-3 md:mb-6 lg:w-1/5 xl:px-4 xl:mb-8">
-      <div className="h-24 bg-gray-400" />
-    </div>
-  );
-}
