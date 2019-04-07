1f9a94d2e0b6780eab1e9a237f6efce4b5d1c6f4
jolaleye <j.olaleye.o@gmail.com>
2019-04-07 12:20:29 -0500

    Add effects to grid

--- components/grid.js
+++ components/grid.js
@@ -1,18 +1,15 @@
-import Effect from './effect';
+import EffectContainer from './EffectContainer';
+import effects from '../effects/index';
 
 export default function Grid() {
-  return (
-    <div className="container flex flex-wrap mx-auto px-2 py-4 md:px-3 lg:px-16 xl:px-32 xl:py-8">
-      <Effect />
-      <Effect />
-      <Effect />
-      <Effect />
-      <Effect />
-      <Effect />
-      <Effect />
-      <Effect />
-      <Effect />
-      <Effect />
-    </div>
-  );
+  const fx = effects.map(effect => {
+    const Effect = effect.el;
+    return (
+      <EffectContainer key={effect.name}>
+        <Effect />
+      </EffectContainer>
+    );
+  });
+
+  return <div className="container flex flex-wrap mx-auto px-2 py-4 md:px-3 lg:px-16 xl:px-32 xl:py-8">{fx}</div>;
 }
