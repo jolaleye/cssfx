65bff773b6e84ca9e9499a6f39e0f8f0dd839a01
jolaleye <j.olaleye.o@gmail.com>
2019-04-06 21:07:59 -0500

    Update navbar style

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,7 @@
 export default function Nav() {
   return (
-    <nav className="border-solid border-b border-gray-300">
-      <div className="container mx-auto p-8">
+    <nav className="lg:mb-4">
+      <div className="container mx-auto py-8 px-6 md:px-10">
         <img className="w-24 md:w-32 lg:w-40" src="/static/logo.svg" alt="" />
       </div>
     </nav>
