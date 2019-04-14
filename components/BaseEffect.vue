86c1c3600a3a2c31c45f9f65539e63e6b345a5f7
jolaleye <j.olaleye.o@gmail.com>
2019-04-13 21:09:16 -0500

    Add BaseEffect component

---
+++ components/BaseEffect.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="outer">
+    <div class="box">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.outer {
+  padding: 0.75em;
+  width: 50%;
+
+  &:hover {
+    background-color: hsla(246, 45%, 15%, 0.5);
+    cursor: pointer;
+  }
+}
+
+.box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media (min-width: "640px") {
+  .outer {
+    width: 33.33%;
+    padding: 1em;
+  }
+}
+
+@media (min-width: "768px") {
+  .outer {
+    width: 25%;
+  }
+}
+
+@media (min-width: "1024px") {
+  .outer {
+    width: 20%;
+    padding: 1em;
+  }
+}
+</style>
+
