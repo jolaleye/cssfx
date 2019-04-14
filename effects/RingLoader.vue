cee2aeb721ab65777420aaf8d6f06b198ad95311
jolaleye <j.olaleye.o@gmail.com>
2019-04-13 21:09:42 -0500

    Add RingLoader effect

---
+++ effects/RingLoader.vue
@@ -0,0 +1,44 @@
+<template>
+  <svg viewBox="25 25 50 50">
+    <circle cx="50" cy="50" r="20"></circle>
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  width: 5em;
+  transform-origin: center;
+  animation: rotate 2s linear infinite;
+}
+
+circle {
+  fill: none;
+  stroke: hsl(341, 98%, 59%);
+  stroke-width: 2;
+  stroke-dasharray: 1, 200;
+  stroke-dashoffset: 0;
+  stroke-linecap: round;
+  animation: dash 1.5s ease-in-out infinite;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 90, 200;
+    stroke-dashoffset: -35px;
+  }
+  100% {
+    stroke-dashoffset: -125px;
+  }
+}
+</style>
+
