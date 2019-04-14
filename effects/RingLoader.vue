7e171ec8b16cb7a3d2bb942c8ec630a0a6f02f04
jolaleye <j.olaleye.o@gmail.com>
2019-04-14 00:34:26 -0500

    Remove template indent
    Fixes formatting in source code display

--- effects/RingLoader.vue
+++ effects/RingLoader.vue
@@ -1,7 +1,7 @@
 <template>
-  <svg viewBox="25 25 50 50">
-    <circle cx="50" cy="50" r="20"></circle>
-  </svg>
+<svg viewBox="25 25 50 50">
+  <circle cx="50" cy="50" r="20"></circle>
+</svg>
 </template>
 
 <style scoped>
