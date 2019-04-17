443c377302178fd63610a0c4045ee5c8d7a52e53
jolaleye <j.olaleye.o@gmail.com>
2019-04-17 16:13:48 -0500

    Add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,13 @@
 export default {
   css: ['~/assets/main.css'],
-  plugins: ['~/plugins/highlight.js', '~/plugins/clipboard.js']
+  plugins: ['~/plugins/highlight.js', '~/plugins/clipboard.js'],
+  modules: [
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: 'UA-129681195-4',
+        dev: false
+      }
+    ]
+  ]
 };
