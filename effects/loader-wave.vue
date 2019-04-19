daa5d437b377106cda2f0fd55050f6218222b1e3
jolaleye <j.olaleye.o@gmail.com>
2019-04-19 15:36:03 -0500

    Add loader-wave

---
+++ effects/loader-wave.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="balls">
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+</template>
+
+<style scoped>
+.balls {
+  width: 4em;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.balls div {
+  width: 0.8em;
+  height: 0.8em;
+  border-radius: 50%;
+  background-color: #fc2f70;
+  transform: translateY(-125%);
+  animation: wave 0.8s ease-in-out alternate infinite;
+}
+
+.balls div:nth-of-type(1) {
+  animation-delay: -0.4s;
+}
+
+.balls div:nth-of-type(2) {
+  animation-delay: -0.2s;
+}
+
+@keyframes wave {
+  to {
+    transform: translateY(125%);
+  }
+}
+</style>
