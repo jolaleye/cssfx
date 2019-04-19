d226d2b1ab84de118be19fe4e7fb7b01de584ab7
jolaleye <j.olaleye.o@gmail.com>
2019-04-19 15:19:58 -0500

    Add loader-box

---
+++ effects/loader-box.vue
@@ -0,0 +1,44 @@
+<template>
+<div></div>
+</template>
+
+<style scoped>
+div {
+  position: relative;
+  width: 2em;
+  height: 2em;
+  border: 3px solid #3cefff;
+  overflow: hidden;
+  animation: spin 3s ease infinite;
+}
+
+div::before {
+  content: '';
+  position: absolute;
+  top: -3px;
+  left: -3px;
+  width: 2em;
+  height: 2em;
+  background-color: hsla(185, 100%, 62%, 0.75);
+  transform-origin: center bottom;
+  transform: scaleY(1);
+  animation: fill 3s linear infinite;
+}
+
+@keyframes spin {
+  50%,
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes fill {
+  25%,
+  50% {
+    transform: scaleY(0);
+  }
+  100% {
+    transform: scaleY(1);
+  }
+}
+</style>
