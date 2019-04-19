0d38056ff69d36793c6a111da21b5d91f38ebfe0
jolaleye <j.olaleye.o@gmail.com>
2019-04-19 09:41:58 -0500

    Add DiskLoader

---
+++ effects/DiskLoader.vue
@@ -0,0 +1,28 @@
+<template>
+<div></div>
+</template>
+
+<style scoped>
+div {
+  width: 3em;
+  height: 4em;
+  border: 3px solid transparent;
+  border-top-color: #fc2f70;
+  border-bottom-color: #fc2f70;
+  border-radius: 50%;
+  animation: stretch 1s linear alternate infinite, spin 1s ease infinite;
+}
+
+@keyframes stretch {
+  to {
+    width: 1em;
+    height: 1em;
+  }
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
