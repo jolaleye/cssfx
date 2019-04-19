ff4a73e170e663cef84790cd573e71ff0eb4c0f1
jolaleye <j.olaleye.o@gmail.com>
2019-04-18 19:57:18 -0500

    Add RingLoader2

---
+++ effects/RingLoader2.vue
@@ -0,0 +1,20 @@
+<template>
+<div></div>
+</template>
+
+<style scoped>
+div {
+  border: 3px solid hsla(0, 0%, 100%, 0.1);
+  border-top: 3px solid #3cefff;
+  border-radius: 50%;
+  width: 4.25em;
+  height: 4.25em;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
