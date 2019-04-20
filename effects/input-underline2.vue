eed87f641bd551ece47b7cf2cc7cf3e48907f79e
jolaleye <j.olaleye.o@gmail.com>
2019-04-20 15:01:47 -0500

    Add input-underline2

---
+++ effects/input-underline2.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <input type="text" placeholder="Input">
+  <span></span>
+</div>
+</template>
+
+<style scoped>
+div {
+  position: relative;
+}
+
+input {
+  width: 5.5em;
+  color: white;
+  font-size: inherit;
+  font-family: inherit;
+  background-color: transparent;
+  border: 1px solid transparent;
+  border-bottom-color: hsla(341, 97%, 59%, 0.3);
+}
+
+input:focus {
+  outline: none;
+}
+
+input::placeholder {
+  color: hsla(0, 0%, 100%, 0.75);
+}
+
+span {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  width: 100%;
+  height: 1px;
+  opacity: 0;
+  background-color: #fc2f70;
+  transform-origin: center;
+  transform: translate(-50%, 0) scaleX(0);
+  transition: all 0.3s ease;
+}
+
+input:focus ~ span {
+  transform: translate(-50%, 0) scaleX(1);
+  opacity: 1;
+}
+</style>
