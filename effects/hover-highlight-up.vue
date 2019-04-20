6b1f3f744107a3c6b389ea83eed5ce16f41d6b42
jolaleye <j.olaleye.o@gmail.com>
2019-04-19 20:37:53 -0500

    Update hover-highlight-up

--- effects/hover-highlight-up.vue
+++ effects/hover-highlight-up.vue
@@ -12,15 +12,18 @@ span::before {
   content: '';
   position: absolute;
   z-index: -1;
-  top: 85%;
+  top: 0;
+  bottom: 0;
   left: -0.25em;
   right: -0.25em;
-  bottom: 0;
-  background-color: hsla(341, 97%, 59%, 0.75);
-  transition: top 0.1s ease-in-out;
+  background-color: #fc2f70;
+  transform-origin: bottom center;
+  transform: scaleY(0.1);
+  transition: all 0.1s ease-in-out;
 }
 
 span:hover::before {
-  top: 0;
+  transform: scaleY(1);
+  background-color: hsla(341, 97%, 59%, 0.75);
 }
 </style>
