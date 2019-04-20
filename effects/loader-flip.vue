ce64ff534c4ff6a63894d2f38d0746a25d8adf88
jolaleye <j.olaleye.o@gmail.com>
2019-04-20 11:45:20 -0500

    Add loader-flip

---
+++ effects/loader-flip.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="box">
+  <div class="plane"></div>
+</div>
+</template>
+
+<style scoped>
+.box {
+  perspective: 120px;
+}
+
+.plane {
+  width: 2em;
+  height: 2em;
+  background-color: #fc2f70;
+  transform: rotate(0);
+  animation: flip 1s infinite;
+}
+
+@keyframes flip {
+  50% {
+    transform: rotateY(180deg);
+  }
+  100% {
+    transform: rotateY(180deg) rotateX(180deg);
+  }
+}
+</style>
