3a1689f6dd25b5713764fb7df52d4b216d226977
jolaleye <j.olaleye.o@gmail.com>
2019-04-19 19:54:38 -0500

    Add hover-highlight-up

---
+++ effects/hover-highlight-up.vue
@@ -0,0 +1,26 @@
+<template>
+<span>Hover</span>
+</template>
+
+<style scoped>
+span {
+  position: relative;
+  z-index: 1;
+}
+
+span::before {
+  content: '';
+  position: absolute;
+  z-index: -1;
+  top: 85%;
+  left: -0.25em;
+  right: -0.25em;
+  bottom: 0;
+  background-color: hsla(341, 97%, 59%, 0.75);
+  transition: top 0.1s ease-in-out;
+}
+
+span:hover::before {
+  top: 0;
+}
+</style>
