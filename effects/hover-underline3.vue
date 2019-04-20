940c2893f4152f5ce287bd9facf3946ddb63aaea
jolaleye <j.olaleye.o@gmail.com>
2019-04-19 20:25:48 -0500

    Add hover-underline3

---
+++ effects/hover-underline3.vue
@@ -0,0 +1,28 @@
+<template>
+<span>Hover</span>
+</template>
+
+<style scoped>
+span {
+  position: relative;
+}
+
+span::before {
+  content: '';
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  width: 100%;
+  height: 2px;
+  opacity: 0;
+  background-color: #3cefff;
+  transform-origin: center;
+  transform: translate(-50%, 0) scaleX(0);
+  transition: all 0.3s ease-in-out;
+}
+
+span:hover::before {
+  transform: translate(-50%, 0) scaleX(1);
+  opacity: 1;
+}
+</style>
