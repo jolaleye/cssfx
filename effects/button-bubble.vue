db93aee60f9238949264635df4dd892eb2d3dbcd
jolaleye <j.olaleye.o@gmail.com>
2019-04-24 18:58:18 -0500

    Add button-bubble

---
+++ effects/button-bubble.vue
@@ -0,0 +1,41 @@
+<template>
+<button>Button</button>
+</template>
+
+<style scoped>
+button {
+  z-index: 1;
+  position: relative;
+  font-size: inherit;
+  font-family: inherit;
+  color: white;
+  padding: 0.5em 1em;
+  outline: none;
+  border: none;
+  background-color: hsl(246, 45%, 15%);
+  overflow: hidden;
+}
+
+button::before {
+  content: '';
+  z-index: -1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  background-color: #fc2f70;
+  transform-origin: center;
+  transform: translate(-50%, -50%) scale(0);
+  transition: transform 0.4s ease-in-out;
+}
+
+button:hover {
+  cursor: pointer;
+}
+
+button:hover::before {
+  transform: translate(-50%, -50%) scale(8);
+}
+</style>
