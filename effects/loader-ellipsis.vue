4d0b56a1cb44f427bb978f4ec55bf51d28443a09
jolaleye <j.olaleye.o@gmail.com>
2019-04-23 19:03:19 -0500

    Add loader-ellipsis

---
+++ effects/loader-ellipsis.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="dots">
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+</template>
+
+<style scoped>
+.dots {
+  width: 3.5em;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.dots div {
+  width: 0.8em;
+  height: 0.8em;
+  border-radius: 50%;
+  background-color: #fc2f70;
+  animation: fade 0.8s ease-in-out alternate infinite;
+}
+
+.dots div:nth-of-type(1) {
+  animation-delay: -0.4s;
+}
+
+.dots div:nth-of-type(2) {
+  animation-delay: -0.2s;
+}
+
+@keyframes fade {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+</style>
