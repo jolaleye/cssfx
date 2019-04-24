f0e08c949ab47de628bec005541fdbe4e8907555
jolaleye <j.olaleye.o@gmail.com>
2019-04-23 20:26:11 -0500

    Add hover-strikethrough

---
+++ effects/hover-strikethrough.vue
@@ -0,0 +1,31 @@
+<template>
+<span>Hover</span>
+</template>
+
+<style scoped>
+span {
+  position: relative;
+}
+
+span::before {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  height: 3px;
+  background-color: #fc2f70;
+  transform-origin: center right;
+  transform: scaleX(0) translateY(-50%);
+  transition: transform 0.3s ease;
+}
+
+span:hover {
+  color: hsla(0, 0%, 100%, 0.8);
+}
+
+span:hover::before {
+  transform-origin: center left;
+  transform: scaleX(1) translateY(-50%);
+}
+</style>
