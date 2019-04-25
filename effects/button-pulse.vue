111088b05497d161fbe9df86f18c06376cdd3dc8
jolaleye <j.olaleye.o@gmail.com>
2019-04-25 17:33:43 -0500

    Add button-pulse

---
+++ effects/button-pulse.vue
@@ -0,0 +1,41 @@
+<template>
+<button>Button</button>
+</template>
+
+<style scoped>
+button {
+  z-index: 1;
+  position: relative;
+  font-size: inherit;
+  font-family: inherit;
+  color: white;
+  padding: 0.5em 1em;
+  outline: none;
+  border: none;
+  background-color: hsl(246, 45%, 15%);
+}
+
+button:hover {
+  cursor: pointer;
+}
+
+button::before {
+  content: '';
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border: 4px solid hsl(246, 45%, 15%);
+  transform-origin: center;
+  transform: scale(1);
+}
+
+button:hover::before {
+  transition: all 0.75s ease-in-out;
+  transform-origin: center;
+  transform: scale(1.75);
+  opacity: 0;
+}
+</style>
