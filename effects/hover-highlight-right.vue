7bda1e908fa5a471be8bb5a9e5f2ac6802da33d0
jolaleye <j.olaleye.o@gmail.com>
2019-04-27 10:26:23 -0500

    Add hover-highlight-right

---
+++ effects/hover-highlight-right.vue
@@ -0,0 +1,29 @@
+<template>
+<span>Hover</span>
+</template>
+
+<style scoped>
+span {
+  position: relative;
+  z-index: 1;
+}
+
+span::before {
+  content: '';
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  bottom: 0;
+  left: -0.25em;
+  right: -0.25em;
+  background-color: hsla(341, 97%, 59%, 0.75);
+  transform-origin: center right;
+  transform: scaleX(0);
+  transition: transform 0.2s ease-in-out;
+}
+
+span:hover::before {
+  transform: scaleX(1);
+  transform-origin: center left;
+}
+</style>
