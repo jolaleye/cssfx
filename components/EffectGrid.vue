394065883ebca0b71f81572ae341c0702b24797c
jolaleye <j.olaleye.o@gmail.com>
2019-04-26 22:08:06 -0500

    Add shuffle and reset controls

--- components/EffectGrid.vue
+++ components/EffectGrid.vue
@@ -1,30 +1,46 @@
 <template>
-  <div class="grid">
-    <BaseEffect
-      v-for="effectName in Object.keys(this.effects)"
-      v-bind:key="effectName"
-      v-on:inspect="$emit('inspect', effectName)"
-    >
-      <component v-bind:is="effectName"></component>
-    </BaseEffect>
+  <div class="container">
+    <Controls v-on:shuffle="shuffle" v-on:reset="reset"/>
+    <transition-group class="grid" name="shuffle" tag="div">
+      <BaseEffect
+        v-for="effectName in Object.keys(this.effects)"
+        v-bind:key="effectName"
+        v-on:inspect="$emit('inspect', effectName)"
+      >
+        <component v-bind:is="effectName"></component>
+      </BaseEffect>
+    </transition-group>
   </div>
 </template>
 
 <script>
 import BaseEffect from "~/components/BaseEffect";
-import { effects, components } from "~/assets/effects.js";
+import Controls from "~/components/Controls";
+import { effects, components, shuffleEffects } from "~/assets/effects.js";
 
 export default {
-  components: { BaseEffect, ...components },
+  components: { BaseEffect, Controls, ...components },
   data() {
     return {
       effects
     };
+  },
+  methods: {
+    shuffle() {
+      this.effects = shuffleEffects();
+    },
+    reset() {
+      this.effects = effects;
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  padding-bottom: 5em;
+}
+
 .grid {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
@@ -32,13 +48,17 @@ export default {
   grid-row-gap: 2em;
   grid-auto-rows: 6em;
   margin: 0 auto;
-  padding: 2em 1em 5em 1em;
+  padding: 0 1em 0 1em;
+}
+
+.shuffle-move {
+  transition: transform 0.75s ease-out;
 }
 
 @media (min-width: "640px") {
   .grid {
     max-width: 640px;
-    padding: 2em 1.5em 5em 1.5em;
+    padding: 0 1.5em 0 1.5em;
     grid-template-columns: repeat(3, 1fr);
     grid-auto-rows: 8em;
     grid-column-gap: 1.5em;
@@ -56,7 +76,7 @@ export default {
 @media (min-width: "1024px") {
   .grid {
     max-width: 1024px;
-    padding: 2em 2em 5em 2em;
+    padding: 0 2em 0 2em;
     grid-template-columns: repeat(5, 1fr);
   }
 }
@@ -64,7 +84,7 @@ export default {
 @media (min-width: "1280px") {
   .grid {
     max-width: 1280px;
-    padding: 2em 3em 5em 3em;
+    padding: 0 3em 0 3em;
     grid-template-columns: repeat(6, 1fr);
   }
 }
