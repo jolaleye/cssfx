62b8788430f091829e08caae2f546d52de5bb9a6
jolaleye <j.olaleye.o@gmail.com>
2019-04-26 22:06:38 -0500

    Add shuffleEffects function

--- assets/effects.js
+++ assets/effects.js
@@ -26,4 +26,25 @@ for (const filename of effectList) {
   components[name] = component;
 }
 
-export { effects, components };
+function shuffleEffects() {
+  const copy = Object.entries(effects).slice(0);
+  const length = copy.length;
+
+  let index = -1;
+  let lastIndex = length - 1;
+  while (++index < length) {
+    const randomIndex = index + Math.floor(Math.random() * (lastIndex - index + 1));
+    const value = copy[randomIndex];
+    copy[randomIndex] = copy[index];
+    copy[index] = value;
+  }
+
+  const newEffects = {};
+  for (const effect of copy) {
+    newEffects[effect[0]] = effect[1];
+  }
+
+  return newEffects;
+}
+
+export { effects, components, shuffleEffects };
