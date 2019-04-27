06c04044b678789f95262bf1fd10973ff02dabc3
jolaleye <j.olaleye.o@gmail.com>
2019-04-27 08:27:23 -0500

    Add loader-coin

---
+++ effects/loader-coin.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="box">
+  <div class="coin"></div>
+</div>
+</template>
+
+<style scoped>
+.box {
+  perspective: 120px;
+}
+
+.coin {
+  width: 2em;
+  height: 2em;
+  border-radius: 50%;
+  border: 4px solid #3cefff;
+  animation: spin 1.5s ease-in-out infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotateY(540deg);
+  }
+}
+</style>
