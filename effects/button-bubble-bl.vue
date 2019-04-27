171b0c3030a43fb9aae5689bc423bde4a1e5b6dc
jolaleye <j.olaleye.o@gmail.com>
2019-04-26 20:41:28 -0500

    Add button-bubble from each corner

---
+++ effects/button-bubble-bl.vue
@@ -0,0 +1,43 @@
+<template>
+<button>Button</button>
+</template>
+
+<style scoped>
+button {
+  z-index: 1;
+  position: relative;
+  font-size: inherit;
+  font-family: inherit;
+  color: white;
+  padding: 0.5em 1em;
+  outline: none;
+  border: none;
+  background-color: hsl(246, 45%, 15%);
+  overflow: hidden;
+  transition: color 0.4s ease-in-out;
+}
+
+button::before {
+  content: '';
+  z-index: -1;
+  position: absolute;
+  top: 100%;
+  right: 100%;
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  background-color: #3cefff;
+  transform-origin: center;
+  transform: translate(50%, -50%) scale(0);
+  transition: transform 0.45s ease-in-out;
+}
+
+button:hover {
+  cursor: pointer;
+  color: #161616;
+}
+
+button:hover::before {
+  transform: translate(50%, -50%) scale(15);
+}
+</style>
