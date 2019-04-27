ce9a62fb4bbea2544f0a96196e93e20a94ec05e7
jolaleye <j.olaleye.o@gmail.com>
2019-04-26 20:15:59 -0500

    Add button-jelly

---
+++ effects/button-jelly.vue
@@ -0,0 +1,37 @@
+<template>
+<button>Button</button>
+</template>
+
+<style scoped>
+button {
+  z-index: 1;
+  font-size: inherit;
+  font-family: inherit;
+  color: white;
+  padding: 0.5em 1em;
+  outline: none;
+  border: none;
+  background-color: hsl(246, 45%, 15%);
+}
+
+button:hover {
+  cursor: pointer;
+  animation: jelly 0.5s;
+}
+
+@keyframes jelly {
+  0%,
+  100% {
+    transform: scale(1, 1);
+  }
+  25% {
+    transform: scale(0.9, 1.1);
+  }
+  50% {
+    transform: scale(1.1, 0.9);
+  }
+  75% {
+    transform: scale(0.95, 1.05);
+  }
+}
+</style>
