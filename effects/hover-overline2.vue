b653a9e1ebef4fa7c36db00e6e6db675172574bf
jolaleye <j.olaleye.o@gmail.com>
2019-04-27 10:16:06 -0500

    Add hover-overline2

---
+++ effects/hover-overline2.vue
@@ -0,0 +1,28 @@
+<template>
+<span>Hover</span>
+</template>
+
+<style scoped>
+span {
+  position: relative;
+}
+
+span::before {
+  content: '';
+  position: absolute;
+  top: -2px;
+  left: 50%;
+  width: 100%;
+  height: 2px;
+  opacity: 0;
+  background-color: #fc2f70;
+  transform-origin: center;
+  transform: translate(-50%, 0) scaleX(0);
+  transition: all 0.3s ease-in-out;
+}
+
+span:hover::before {
+  transform: translate(-50%, 0) scaleX(1);
+  opacity: 1;
+}
+</style>
