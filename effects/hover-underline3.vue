21ae93a65c898ccdab3b712de511891f78561e78
jolaleye <j.olaleye.o@gmail.com>
2019-04-27 12:35:21 -0500

    Update hover-underline3

--- effects/hover-underline3.vue
+++ effects/hover-underline3.vue
@@ -14,15 +14,13 @@ span::before {
   bottom: 0;
   width: 100%;
   height: 2px;
-  opacity: 0;
   background-color: #fc2f70;
   transform-origin: center;
   transform: translate(-50%, 0) scaleX(0);
-  transition: all 0.3s ease-in-out;
+  transition: transform 0.3s ease-in-out;
 }
 
 span:hover::before {
   transform: translate(-50%, 0) scaleX(1);
-  opacity: 1;
 }
 </style>
