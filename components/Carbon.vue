d243746f5935ccf02fd8fbf437d732234feb38ab
jolaleye <j.olaleye.o@gmail.com>
2019-05-10 15:16:41 -0500

    Add carbon ad

---
+++ components/Carbon.vue
@@ -0,0 +1,70 @@
+<template>
+  <script
+    id="_carbonads_js"
+    async
+    type="text/javascript"
+    src="//cdn.carbonads.com/carbon.js?serve=CK7DVKQN&placement=cssfxdev"
+  ></script>
+</template>
+
+<style>
+#carbonads {
+  display: block;
+  overflow: hidden;
+  margin: 0 auto;
+  padding: 10px;
+  box-shadow: 0 1px 3px hsla(0, 0%, 0%, 0.05);
+  border-radius: 4px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Helvetica, Arial,
+    sans-serif;
+  line-height: 1.5;
+  max-width: 300px;
+  font-size: 12px;
+  /* background-color: #fff; */
+  background-color: hsl(246, 41%, 21%);
+  color: white;
+  transform: scale(0.9);
+}
+
+#carbonads a {
+  text-decoration: none;
+  color: inherit;
+}
+
+#carbonads span {
+  position: relative;
+  display: block;
+  overflow: hidden;
+}
+
+.carbon-img {
+  float: left;
+  margin-right: 1em;
+}
+
+.carbon-img img {
+  display: block;
+}
+
+.carbon-text {
+  display: block;
+  float: left;
+  max-width: calc(100% - 130px - 1em);
+  text-align: left;
+  color: #637381;
+}
+
+.carbon-poweredby {
+  position: absolute;
+  left: 142px;
+  bottom: 0;
+  display: block;
+  font-size: 8px;
+  color: #c5cdd0;
+  font-weight: 500;
+  text-transform: uppercase;
+  line-height: 1;
+  letter-spacing: 1px;
+}
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -2,12 +2,22 @@
   <header>
     <h1>Beautifully simple click-to-copy CSS effects</h1>
     <p>A carefully crafted collection designed with a focus on fluidity, simplicity, and ease of use. Powered by CSS with minimal markup. Completely open source and MIT licensed.</p>
+    <Carbon/>
   </header>
 </template>
 
+<script>
+import Carbon from "~/components/Carbon";
+
+export default {
+  components: { Carbon }
+};
+</script>
+
+
 <style lang="scss" scoped>
 header {
-  padding: 1.5em 1.25em;
+  padding: 1.5em 1.25em 0.75em 1.25em;
   text-align: center;
 }
 
@@ -17,25 +27,29 @@ h1 {
   letter-spacing: 0.03em;
 }
 
+p {
+  margin-bottom: 2em;
+}
+
 @media (min-width: "640px") {
   header {
     max-width: 640px;
     margin: 0 auto;
-    padding: 1.75em 3em;
+    padding: 1.75em 3em 0.75em 3em;
   }
 }
 
 @media (min-width: "768px") {
   header {
     max-width: 768px;
-    padding: 2em 3em;
+    padding: 2em 3em 0.75em 3em;
   }
 }
 
 @media (min-width: "1024px") {
   header {
     max-width: 850px;
-    padding: 3em;
+    padding: 3em 3em 0.75em 3em;
   }
 
   h1 {
