d0f271098f6fc16d4a0f89d52ab0431c43cf4263
jolaleye <j.olaleye.o@gmail.com>
2019-05-13 21:43:43 -0500

    Comment out old index page

--- pages/index.old.vue
+++ pages/index.old.vue
@@ -1,45 +1,45 @@
 <template>
   <div>
-    <Navbar/>
+    <!-- <Navbar/>
     <Header/>
     <EffectGrid v-on:inspect="inspect"/>
-    <InspectModal v-if="inspecting" v-bind:src="inspectSrc" v-on:close="closeInspect"/>
+    <InspectModal v-if="inspecting" v-bind:src="inspectSrc" v-on:close="closeInspect"/>-->
   </div>
 </template>
 
 <script>
-import Navbar from "~/components/Navbar.old";
-import Header from "~/components/Header.old";
-import EffectGrid from "~/components/EffectGrid.old";
-import InspectModal from "~/components/InspectModal.old";
-import { effects } from "~/assets/effects.js";
+// import Navbar from "~/components/Navbar.old";
+// import Header from "~/components/Header.old";
+// import EffectGrid from "~/components/EffectGrid.old";
+// import InspectModal from "~/components/InspectModal.old";
+// import { effects } from "~/assets/effects.js";
 
-export default {
-  components: { Navbar, Header, EffectGrid, InspectModal },
-  data() {
-    return {
-      effects,
-      inspecting: false,
-      inspectSrc: {}
-    };
-  },
-  methods: {
-    inspect(effectName) {
-      this.inspecting = true;
-      this.inspectSrc = this.effects[effectName];
-    },
-    closeInspect() {
-      this.inspecting = false;
-      this.inspectSrc = {};
-    }
-  },
+// export default {
+//   components: { Navbar, Header, EffectGrid, InspectModal },
+//   data() {
+//     return {
+//       effects,
+//       inspecting: false,
+//       inspectSrc: {}
+//     };
+//   },
+//   methods: {
+//     inspect(effectName) {
+//       this.inspecting = true;
+//       this.inspectSrc = this.effects[effectName];
+//     },
+//     closeInspect() {
+//       this.inspecting = false;
+//       this.inspectSrc = {};
+//     }
+//   },
 
-  head() {
-    return {
-      bodyAttrs: {
-        class: this.inspecting ? "inspecting" : ""
-      }
-    };
-  }
-};
+//   head() {
+//     return {
+//       bodyAttrs: {
+//         class: this.inspecting ? "inspecting" : ""
+//       }
+//     };
+//   }
+// };
 </script>
